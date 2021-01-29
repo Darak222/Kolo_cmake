c520abc3b4ff1e80b9582db21986fade0316baea
Darak222 <74760592+Darak222@users.noreply.github.com>
2021-01-29 09:05:30 +0100

    Add files via upload

---
+++ kol_1_makefile_cmakefile/zadanie3/bin/CMakeFiles/myProgram.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/darak/kolo_snwo/kol_1/zadanie3/src/address.cpp" "/home/darak/kolo_snwo/kol_1/zadanie3/bin/CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/address.cpp.o"
+  "/home/darak/kolo_snwo/kol_1/zadanie3/src/employee.cpp" "/home/darak/kolo_snwo/kol_1/zadanie3/bin/CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/employee.cpp.o"
+  "/home/darak/kolo_snwo/kol_1/zadanie3/src/main.cpp" "/home/darak/kolo_snwo/kol_1/zadanie3/bin/CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kol_1_makefile_cmakefile/zadanie3/bin/CMakeFiles/myProgram.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/address.cpp.o"
+  "CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/employee.cpp.o"
+  "CMakeFiles/myProgram.dir/home/darak/kolo_snwo/kol_1/zadanie3/src/main.cpp.o"
+  "myProgram"
+  "myProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
